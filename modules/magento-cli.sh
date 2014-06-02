4a24fe17d1418335abf06e7796d0ab2810061b6b
Volodymyr Dubyna <vdubyna@speroteck.com>
2014-06-02 12:22:02 +0300

    Fix problems

--- modules/magento-cli.sh
+++ modules/magento-cli.sh
@@ -14,5 +14,6 @@ echo "-> Install"
     mv -v /usr/bin/composer.phar /usr/bin/composer
     composer config --global repositories.speroteck composer http://composer.speroteck.com/
     composer config --global repositories.firegento composer http://packages.firegento.com/
+    composer config -g process-timeout 3600
 
 echo "-> Success"

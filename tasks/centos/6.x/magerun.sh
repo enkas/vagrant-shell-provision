5fc1bdf61d9079fd4f6fc474e97298dc1f2f00c4
Volodymyr Dubyna <vdubyna@speroteck.com>
2014-12-23 20:35:34 +0200

    Add magerun task

---
+++ tasks/centos/6.x/magerun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "NAME: Install Magerun"
+
+echo "-> PREPARE"
+
+    if [[ $(which magerun) == */magerun ]]; then
+        echo "Mage CLI already installed"
+        exit 0
+    fi
+    cd /tmp
+
+echo "-> START"
+
+    wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
+    chmod +x ./n98-magerun.phar
+    sudo mv ./n98-magerun.phar /usr/bin/magerun
+
+echo "-> FINISH"

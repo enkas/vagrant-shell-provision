411eaceb8bffb5c4cd4db1e637550ea5937e29a3
Volodymyr Dubyna <vdubyna@speroteck.com>
2014-12-23 20:23:48 +0200

    add composer install

---
+++ tasks/centos/6.x/composer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "NAME: Install Composer"
+
+echo "-> PREPARE"
+
+    if [[ $(which composer) == */composer ]]; then
+        echo "Composer already installed"
+        exit 0
+    fi
+    cd /tmp
+
+echo "-> START"
+
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin
+    mv -v /usr/bin/composer.phar /usr/bin/composer
+    composer config --global repositories.speroteck composer http://composer.speroteck.com/
+    composer config --global repositories.firegento composer http://packages.firegento.com/
+    composer config -g process-timeout 3600
+
+echo "-> FINISH"
